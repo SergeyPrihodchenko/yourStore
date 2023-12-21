759c7b832e0d1156231d1cd1b4c81aabbcc66e8c
Sergo <S.prihodchenko@yandex.ru>
2023-12-21 17:20:28 +0300

    a

---
+++ app/Models/admin/product/Option.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models\admin\product;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Option extends Model
+{
+    use HasFactory;
+
+    protected $timestamps = false;
+
+    public function values(): HasMany
+    {
+        return $this->hasMany(OptionValues::class, 'option_id');
+    }   
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class, 'product_options', 'option_id', 'product_id');
+    }
+}
---
+++ app/Models/admin/product/ProductOption.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\admin\product;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductOption extends Model
+{
+    use HasFactory;
+}
