41a10adf145c631def00d0ad8922c9d0c10a9f98
Katherine <strix1112@gmail.com>
2024-01-08 20:36:50 +0300

    created CustomLink

--- resources/js/Layouts/MainLayout/ui/AdminTabs/AdminTabs.tsx
+++ resources/js/Layouts/MainLayout/ui/AdminTabs/AdminTabs.tsx
@@ -1,65 +1,26 @@
-import Box from "@mui/material/Box";
-import Tabs from "@mui/material/Tabs";
-import {Tab} from "@mui/material";
-import {SyntheticEvent, useState} from "react";
-import {router} from "@inertiajs/react";
+import s from './AdminTabs.module.css';
+import CustomLink, {VariantLink} from "@/Components/CustomLink/CustomLink";
+
 interface  AdminTabsProps{
 
 }
 
-console.log('log',route().current('admin.product'))
+console.log('current route',route().current('admin.product'))
 
-interface LinkTabProps {
-    label?: string;
-    href?: string;
-    selected?: boolean;
 
-}
-function LinkTab(props: LinkTabProps) {
-    console.log('selected',props.selected);
-    return (
-        <Tab
-            component="a"
-            aria-current={props.selected && 'page'}
-            {...props}
-        />
-    );
-}
-const paths = [
-    {
-        name: 'Dashboard',
-        path: "dashboard"
-    },
-    {
-        name: 'Product',
-        path: "admin.product"
-    },
 
-];
+
 export const AdminTabs = (props: AdminTabsProps) =>{
-    const [value, setValue] = useState(0);
-    const handleChange = (event: SyntheticEvent, newValue: number) => {
-        console.log(event.target)
-        setValue(newValue);
-    };
-    const handleTabClick = (routeStr: string) => {
-        router.get(route(routeStr));
-    }
+
     return (
-        <Box sx={{ width: '100%' }}>
-            <Tabs
-                value={value}
-                onChange={handleChange}
-                aria-label="nav tabs example"
-                role="navigation"
-            >
+        <ul className={s.adminPanelList}>
+            <li className={s.adminPanelItem}>
+                <CustomLink href={route('dashboard')} variant={VariantLink.DEFAULT} active={route().current('dashboard')} >Dashboard</CustomLink>
+            </li>
+            <li className={s.adminPanelItem}>
+                <CustomLink href={route('admin.product')} variant={VariantLink.DEFAULT} active={route().current('admin.product')} >Добавить продукт</CustomLink>
+            </li>
+        </ul>
 
-                <Tab component={'a'} aria-current={route().current('dashboard') && 'page'} label={"Dashboard"} onClick={()=> router.get(route("dashboard"))} />
-                {/*<Tab label={"Product"} onClick={()=> handleTabClick('admin.product')} value={1} />*/}
-                <Tab component={'a'} aria-current={route().current('admin.product') && 'page'} label={"product"} onClick={()=> router.get(route("admin.product"))} />
-                {/*{paths.map(path => <LinkTab key={path.name} label={path.name}  onClick={()=>handleTabClick(path.path)}  /> )}*/}
-
-            </Tabs>
-        </Box>
     );
 }
