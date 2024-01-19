7bebae11f96822c7b5c970c4493d6003d3b593f3
Sergo <S.prihodchenko@yandex.ru>
2024-01-19 14:21:17 +0300

    a

--- resources/js/Pages/Admin/Option/ui/OptionComponent.tsx
+++ resources/js/Pages/Admin/Option/ui/OptionComponent.tsx
@@ -1,12 +1,39 @@
-import { Grid } from "@mui/material";
+import TableComponent from "@/Components/MuiComponents/TableComponent";
+import { router } from "@inertiajs/react";
+import { Button, Grid, TextField } from "@mui/material";
+import { useState } from "react";
 
 const OptionComponent = ({options}: any) => {
-    console.log(options);
+
+    const [valueTitle, setValueTitle] = useState('');
+
+    const handleChangeTitle = (e: any) => {
+        setValueTitle(e.target.value);
+    } 
+    
+    const setOption = () => {
+        router.post(route('setOption'), {title: valueTitle}, {
+            onSuccess: (result) => {
+                setValueTitle('');
+                router.reload({only: ['options']});
+            },
+            onError: (err) => {
+                console.log(err);
+            }
+        });
+    }
     
     return (
         <Grid container>
-            <Grid item>
-
+            <Grid item xs={12}>
+                <TextField variant="outlined" label="Название опции" helperText="Правила добавления" onChange={handleChangeTitle} value={valueTitle}/>
+                <Button variant="outlined" onClick={setOption}>Добавить</Button>
+            </Grid>
+            <Grid item xs={6}>
+                <TableComponent rows={options}/>
+            </Grid>
+            <Grid item xs={6}>
+                <TableComponent rows={options}/>
             </Grid>
         </Grid>
     );
