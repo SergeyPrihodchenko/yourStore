b37cfbb34ee9c544e9995e27dc8c40408d1bd167
Sergo <S.prihodchenko@yandex.ru>
2024-02-12 16:28:42 +0300

    a

--- resources/js/Pages/Admin/Product/ui/ProductShowOptinComponent.tsx
+++ resources/js/Pages/Admin/Product/ui/ProductShowOptinComponent.tsx
@@ -1,11 +1,26 @@
-import { Grid, ImageList, ImageListItem, Typography } from "@mui/material";
+import { Box, Grid, ImageList, ImageListItem, Typography } from "@mui/material";
 import { AdminProductPanelInterface } from "../model/types";
-import { Option } from "@/Entities/Option/model/types";
+import { Option, Value } from "@/Entities/Option/model/types";
 import CustomizedDialogsForInput from "./components/CustomizedDialogsForInput";
 import EditIcon from '@mui/icons-material/Edit';
 
 const ProductShowOptionComponent = ({catalog, category, product, values, images, options}: AdminProductPanelInterface) => {
-    console.log(values);
+
+    const mergeValues = (options: Option[], values: Value[]) => {
+
+      options.forEach(option => {
+
+        option.values = []
+
+          values.forEach(value => {
+            if(option.id == value.option_id) {
+              option.values?.push(value);
+            }
+          })
+      });
+    }
+
+    mergeValues(options, values);
 
     const optionsDate: any = {
       year: 'numeric',
@@ -24,17 +39,19 @@ const ProductShowOptionComponent = ({catalog, category, product, values, images,
     
     
     return (
-        <Grid container sx={{padding: '50px'}}>
-            <Grid item xs={12}>
+        <Grid container sx={{padding: '20px 0'}}>
+        <Grid item xs={12}>
       <Typography variant="h2" gutterBottom>
         {product.title} <CustomizedDialogsForInput><EditIcon sx={{color: '#363636'}}/></CustomizedDialogsForInput>
       </Typography>
+      <Box sx={{boxShadow: '0 5px 20px', borderRadius: '16px', padding: '5px', margin: '10px 0'}}>
       <Typography variant="h3" gutterBottom>
-        Категория: {category.title}
+        Категория:<br/> {category.title}
       </Typography>
       <Typography variant="h4" gutterBottom>
-        Подкатегория: {catalog.title} 
+        Подкатегория:<br/> {catalog.title} 
       </Typography>
+      </Box>
       <ImageList sx={{ width: '100%', height: 250 }} cols={4} rowHeight={200}>
       {images.map((item) => (
         <ImageListItem key={item.id}>
@@ -56,15 +73,20 @@ const ProductShowOptionComponent = ({catalog, category, product, values, images,
       <Typography variant="body1" gutterBottom>
         Описание:<br/> {product.description}
       </Typography>
+      <br/>
       <Typography variant="body1" gutterBottom>
         Дата создания: {dateUpdate}
       </Typography>
-       {options && options.map((option: Option) => {return (
-          <Typography variant="body1" gutterBottom>{option.title}</Typography>
-       )})}
-       {values && values.map((option: Option) => {return (
-          <Typography variant="body1" gutterBottom>{option.title}</Typography>
-       )})}
+      <br/>
+       <Box sx={{border: 'solid 1px black', position: 'relative'}}>
+       {options?.map((option: Option) => (
+        <>
+          <Typography key={option.id} variant="body1" gutterBottom>{option.title} :</Typography>
+          {option.values?.map(value => (<p key={value.id}>{value.title}</p>))}
+          <br/>
+        </>
+       ))}
+       </Box>
             </Grid>
         </Grid>
     );
