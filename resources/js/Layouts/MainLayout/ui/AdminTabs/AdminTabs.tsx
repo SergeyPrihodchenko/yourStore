e8fa7abf458b9f010561708d761ed4c53ee26031
Ekaterina Savchenko <strix1112@gmail.com>
2024-02-16 20:19:14 +0300

    MobileAdminPanel

--- resources/js/Layouts/MainLayout/ui/AdminTabs/AdminTabs.tsx
+++ resources/js/Layouts/MainLayout/ui/AdminTabs/AdminTabs.tsx
@@ -1,28 +1,41 @@
+import { useState } from 'react';
+import cn from 'classnames';
 import s from './AdminTabs.module.css';
 import CustomLink, {VariantLink} from "@/Components/CustomLink/CustomLink";
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 
 interface  AdminTabsProps{}
 
 const AdminTabs = (props: AdminTabsProps) =>{
+  const [showMobilePanel, setShowMobilePanel] = useState(false);
 
-    return (
-        <ul className={s.adminPanelList}>
+  return (
+    <>
+      <div>
+        <button onClick={()=>setShowMobilePanel(prev => !prev)} type='button' className={s.adminPanelBtn}>
+          <AdminPanelSettingsIcon fontSize='large' /> Панель администратора
+        </button>
+      </div>
+      <ul className={cn(s.adminPanelList, {
+        [s.showPanel]: showMobilePanel
+      })}>
             <li className={s.adminPanelItem}>
-                <CustomLink href={route('dashboard')} variant={VariantLink.DEFAULT} active={route().current('dashboard')} >Dashboard</CustomLink>
+                <CustomLink href={route('dashboard')} variant={VariantLink.DEFAULT} active={route().current('dashboard')} className='w-full'>Dashboard</CustomLink>
             </li>
             <li className={s.adminPanelItem}>
-                <CustomLink href={route('admin.product')} variant={VariantLink.DEFAULT} active={route().current('admin.product')} >Продукты</CustomLink>
+                <CustomLink href={route('admin.product')} variant={VariantLink.DEFAULT} active={route().current('admin.product')} className='w-full'>Продукты</CustomLink>
             </li>
             <li className={s.adminPanelItem}>
-                <CustomLink href={route('category.index')} variant={VariantLink.DEFAULT} active={route().current('category.index')} >Категории</CustomLink>
+                <CustomLink href={route('category.index')} variant={VariantLink.DEFAULT} active={route().current('category.index')} className='w-full' >Категории</CustomLink>
             </li>
             <li className={s.adminPanelItem}>
-                <CustomLink href={route('catalog.index')} variant={VariantLink.DEFAULT} active={route().current('catalog.index')} >Подкатегории</CustomLink>
+                <CustomLink href={route('catalog.index')} variant={VariantLink.DEFAULT} active={route().current('catalog.index')} className='w-full'>Подкатегории</CustomLink>
             </li>
             <li className={s.adminPanelItem}>
-                <CustomLink href={route('option')} variant={VariantLink.DEFAULT} active={route().current('option')} >Опции</CustomLink>
+                <CustomLink href={route('option')} variant={VariantLink.DEFAULT} active={route().current('option')} className='w-full'>Опции</CustomLink>
             </li>
         </ul>
+        </>
 
     );
 }
