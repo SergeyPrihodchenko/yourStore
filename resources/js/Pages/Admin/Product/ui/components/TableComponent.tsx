49fdc3e6d4367833b856aeb84c445245fdd4f2fd
Sergo <S.prihodchenko@yandex.ru>
2024-02-26 12:13:55 +0300

    a

--- resources/js/Pages/Admin/Product/ui/components/TableComponent.tsx
+++ resources/js/Pages/Admin/Product/ui/components/TableComponent.tsx
@@ -6,19 +6,27 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Option, Value } from '@/Entities/Option/model/types';
+import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
 import { useState } from 'react';
+import { Button } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
 
 
 export default function TableComponent({options, values}: any) {
     
     const [rows, setRows] = useState(options)
-
+    const [toggle, setToggle] = useState<boolean>(false);
     const filterValueForOption = (id: number) => {
         const filtredValues = values.filter((value: Value) => {
             return value.option_id == id
         })
-
         setRows(filtredValues);
+        setToggle(true);
+    }
+
+    const resetFilter = () => {
+      setRows(options);
+      setToggle(false);
     }
     
   return (
@@ -26,20 +34,35 @@ export default function TableComponent({options, values}: any) {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Фильтры</TableCell>
+            <TableCell>{!toggle? 'Фильтры' : 'Параметры фильтра'}</TableCell>
+            {!toggle ? '' : <TableCell>Количество едениц</TableCell>}
             <TableCell align="right">Действия</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+        {!toggle ? 
+            ''
+        : 
+          <TableHead>
+          <TableRow>
+            <TableCell sx={{'&:hover': {background:'#949494', cursor: 'pointer'}}} onClick={resetFilter}><ArrowBackIosOutlinedIcon/><span>назад</span></TableCell>
+          </TableRow>
+          </TableHead> 
+        }
+          {rows.map((row: any) => (
             <TableRow
               key={row.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 }}}
             >
-              <TableCell component="th" scope="row" sx={{'&:hover': {background: 'blue', cursor: 'pointer'}}} onClick={() => {filterValueForOption(row.id)}}>
+              <TableCell component="th" scope="row" sx={{'&:hover': {background:'#949494', cursor: 'pointer'},  display: 'flex', justifyContent: 'space-between'}} onClick={() => {filterValueForOption(row.id)}}>
                 {row.title}
+                <EditIcon/>
               </TableCell>
-              <TableCell align="right">test</TableCell>
+              {!toggle ? '' : <TableCell component="th" scope="row">1</TableCell>}
+              <TableCell align="right">
+                <Button variant='outlined'>Изменить</Button>
+                <Button variant='outlined'>Удалить</Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
